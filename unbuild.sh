9d16d143e0525bd05455695f84b3f93aaca9ee9e
lietk12@gmail.com <ethanjli>
2017-05-26 02:53:42 -0700

    Mock unbuild script.

---
+++ unbuild.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ROOT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+UNBUILD_PATH="${ROOT_PATH}/unbuild"
+RECONSTRUCT_OUTPUT_PATH="${UNBUILD_PATH}/src"
+
+mkdir -p "$RECONSTRUCT_OUTPUT_PATH"
+date > "$RECONSTRUCT_OUTPUT_PATH/date.txt"
+
