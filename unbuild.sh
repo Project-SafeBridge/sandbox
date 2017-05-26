167937a1b7e08edd26278c53763465c48a823eef
lietk12@gmail.com <ethanjli>
2017-05-26 03:08:44 -0700

    Use the unbuild input file.

--- unbuild.sh
+++ unbuild.sh
@@ -5,5 +5,6 @@ UNBUILD_PATH="${ROOT_PATH}/unbuild"
 RECONSTRUCT_OUTPUT_PATH="${UNBUILD_PATH}/src"
 
 mkdir -p "$RECONSTRUCT_OUTPUT_PATH"
-date > "$RECONSTRUCT_OUTPUT_PATH/date.txt"
+date > "${RECONSTRUCT_OUTPUT_PATH}/date.txt"
+cp "${UNBUILD_PATH}/stella.html" "${RECONSTRUCT_OUTPUT_PATH}/stella.html"
 
